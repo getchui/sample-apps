1923dfdf91c3ed7832265724c8c766e4cb66027e
Cyrus Behroozi <cyrus.behr@gmail.com>
2019-10-16 13:57:22 -0700

    Removed age-predict-webapp from pyton_sdk

--- python_sdk/age-predict-webapp/src/Loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactLoading from 'react-loading';
- 
-const Loading = ({ type, color }) => (
-    <ReactLoading type={type} color={color} height={667} width={375} />
-);
- 
-export default Loading;
