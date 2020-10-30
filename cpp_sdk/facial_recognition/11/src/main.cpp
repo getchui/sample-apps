ba06e13e3e3d0bf7606ae1e2ea83310c0913b96a
Cyrus Behroozi <cyrus.behr@gmail.com>
2020-10-30 10:29:06 -0700

    WIP

--- cpp_sdk/facial_recognition/11/src/main.cpp
+++ cpp_sdk/facial_recognition/11/src/main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
-#include <vector>
 
 #include "tf_sdk.h"
 #include "tf_data_types.h"
+#include <fstream>
+#include <istream>
+
+
+std::vector<uint8_t> readToBuffer()
 
 int main() {
+    std::ifstream file("/home/cyrus/Downloads/tfv4_output.bin", std::ios::binary | std::ios::ate);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<uint8_t> buffer(size);
+    if (!file.read(reinterpret_cast<char*>(buffer.data()), size)) {
+        std::cout << "Unable to read file" << std::endl;
+        return -1;
+    }
+
+    for (int i = 0; i < buffer.size(); i+=4) {
+        std::cout << *(reinterpret_cast<float*>((buffer.data() + i))) << " ";
+    }
+    std::cout << "\n";
+
     Trueface::SDK tfSdk;
 
     // TODO: replace <LICENSE_CODE> with your license code.
-    const auto isValid = tfSdk.setLicense("<LICENSE_CODE>");
+    const auto isValid = tfSdk.setLicense("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbW90aW9uIjp0cnVlLCJmciI6dHJ1ZSwiYnlwYXNzX2dwdV91dWlkIjp0cnVlLCJwYWNrYWdlX2lkIjpudWxsLCJleHBpcnlfZGF0ZSI6IjIwMjEtMDEtMDEiLCJncHVfdXVpZCI6W10sInRocmVhdF9kZXRlY3Rpb24iOnRydWUsIm1hY2hpbmVzIjoxLCJhbHByIjp0cnVlLCJuYW1lIjoiQ3lydXMgR1BVIiwidGtleSI6Im5ldyIsImV4cGlyeV90aW1lX3N0YW1wIjoxNjA5NDU5MjAwLjAsImF0dHJpYnV0ZXMiOnRydWUsInR5cGUiOiJvZmZsaW5lIiwiZW1haWwiOiJjeXJ1c0B0cnVlZmFjZS5haSJ9.2uQZTG_AXcHVXEFahbvkM8-gmosLPxjSSnbfEAz5gpY");
     if (!isValid) {
         std::cout << "Error: the provided license is invalid\n";
         return -1;
---
+++ cpp_sdk/facial_recognition/1N_threadpool_cpu/src/main.cpp
@@ -0,0 +1,141 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <memory>
+#include <atomic>
+#include <chrono>
+#include <cmath>
+#include <queue>
+#include <condition_variable>
+
+#include "tf_sdk.h"
+#include "tf_data_types.h"
+
+class Controller {
+public:
+    Controller(const std::string& sdkToken, const std::vector<std::string>& rtspURLs);
+
+private:
+    // Function for connecting to an RTSP stream and enrolling frames into a queue.
+    // Assuming our cameras stream at 30FPS, we will only process every 6th frame
+    // to process at 5FPS because any higher and we end up processing very similar frames
+    // and doing unnecessary work.
+    void grabAndEnqueueFrames(const std::string& rtspURL) {
+        // Open the video capture
+        cv::VideoCapture cap;
+        if (!cap.open(rtspURL)) {
+            auto errMsg = "Unable to open video stream at URL: " + rtspURL;
+            throw std::runtime_error(errMsg);
+        }
+
+        // Main loop
+        while(m_run) {
+            // Only retrieve ever 6th frame from the stream
+            for (auto i = 0; i < 6; ++i) {
+                cap.grab();
+            }
+
+            cv::Mat frame;
+            auto ret = cap.retrieve(frame);
+            if (!ret) {
+                // Unable to retrieve frame
+                continue;
+            }
+
+            // Push a frame to the queue, notify thread pool that there is work
+            {
+                std::lock_guard<std::mutex> lock(m_imageQueueMtx);
+                m_imageQueue.push(std::move(frame));
+            }
+
+            m_imageQueueCondVar.notify_one();
+        }
+
+        std::cout << "Kill command received, rtsp thread shutting down..." << std::endl;
+    }
+
+    // Function for searching for all faces in the frame and pushing the aligned face chips
+    // into another queue to be processed for face recognition
+    void detectAndEnqueueFaces(const std::string& sdkToken, const Trueface::ConfigurationOptions& options) {
+        // Create and initialize a Trueface SDK
+        Trueface::SDK tfSdk(options);
+        auto ret = tfSdk.setLicense(sdkToken);
+        if (!ret) {
+            throw std::runtime_error("Invalid token");
+        }
+
+        // Main loop
+        while(m_run) {
+            cv::Mat image;
+            // Wait for work
+            {
+                std::unique_lock<std::mutex> lock(m_imageQueueMtx);
+                m_imageQueueCondVar.wait(lock, [this]{ return !this->m_imageQueue.empty() || !this->m_run;});
+
+                if (!m_run) {
+                    // Exit signal received
+                    break;
+                }
+
+                image = m_imageQueue.front();
+                m_imageQueue.pop();
+            }
+
+            // Pass the image to the SDK, run face detection
+            // OpenCV uses BGR default
+            auto retcode = tfSdk.setImage(image.data, image.cols, image.rows, Trueface::ColorCode::bgr);
+            if (retcode != Trueface::ErrorCode::NO_ERROR) {
+                std::cout << "Thread " << std::this_thread::get_id() << ": Unable to set image" << std::endl;
+                continue;
+            }
+
+            // Detect all the faces in the image
+            std::vector<Trueface::FaceBoxAndLandmarks> faceBoxAndLandmarks;
+            retcode = tfSdk.detectFaces(faceBoxAndLandmarks);
+
+            if (retcode != Trueface::ErrorCode::NO_ERROR) {
+                std::cout << "Thread " << std::this_thread::get_id() << ": Error detecting faces" << std::endl;
+                continue;
+            }
+
+            // For each detected face, extract the aligned face chip, add to the face chip queue
+            // Each face chip is 112x112 pixels in size, so we must allocate 112x112x3 bytes
+            for (const auto& fb: faceBoxAndLandmarks) {
+                std::vector<uint8_t> faceImage (112*112*3);
+                retcode = tfSdk.extractAlignedFace(fb, faceImage.data());
+
+                if (retcode != Trueface::ErrorCode::NO_ERROR) {
+                    std::cout << "Thread " << std::this_thread::get_id() << ": Error extracting aligned face" << std::endl;
+                    continue;
+                }
+
+                // Push the face image into our queue and indicate that work is ready
+                {
+                    std::lock_guard<std::mutex> lock(m_faceChipQueueMtx);
+                    m_faceChipQueue.push(std::move(faceImage));
+                }
+                m_faceChipQueueCondVar.notify_one();
+            }
+        }
+
+
+    }
+
+    // Shared queues and their mutexes
+    std::queue<cv::Mat> m_imageQueue;
+    std::queue<std::vector<uint8_t>> m_faceChipQueue;
+
+    std::mutex m_imageQueueMtx;
+    std::mutex m_faceChipQueueMtx;
+
+    // Conditional variables for indicating work is ready
+    std::condition_variable m_imageQueueCondVar;
+    std::condition_variable m_faceChipQueueCondVar;
+
+    // When set to false, worker threads should stop running
+    std::atomic<bool> m_run {true};
+};
