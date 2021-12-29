8870dc2dcebff9345165ee46f5999c93c2a70572
Cyrus Behroozi <cyrus.behr@gmail.com>
2021-12-29 14:12:08 -0800

    Added GPU code

--- cpp_sdk/facial_recognition/1N/src/main.cpp
+++ cpp_sdk/facial_recognition/1N/src/main.cpp
@@ -89,8 +89,9 @@ int main() {
     // https://docs.trueface.ai/ROC-Curves-d47d2730cf0a44afacb39aae0ed1b45a
     const float threshold = 0.3;
 
-    // TODO: If you have a NVIDIA gpu, then enable the enableGPU flag (you will require a GPU specific token for this).
     Trueface::ConfigurationOptions options;
+    // TODO: If you have a NVIDIA gpu, then enable the enableGPU flag (you will require a GPU specific token for this).
+//    options.gpuOptions = true;
     options.frModel = Trueface::FacialRecognitionModel::TFV5;
     // Note, if you do use TFV5, you will need to run the download script in /download_models to obtain the model file
     options.dbms = Trueface::DatabaseManagementSystem::NONE; // The data will not persist after the app terminates using this backend option.
