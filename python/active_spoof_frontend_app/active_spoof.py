e9149ac3d3372b27a33041173e8f1dc2b37c8819
cyrus <cyrus.behr@gmail.com>
2023-05-04 18:39:45 -0700

    Fixed active spoof

--- python/active_spoof_frontend_app/active_spoof.py
+++ python/active_spoof_frontend_app/active_spoof.py
@@ -67,10 +67,58 @@ listener = keyboard.Listener(
 listener.start()
 
 
+# Start by specifying the configuration options to be used. 
+# Can choose to use the default configuration options if preferred by calling the default SDK constructor.
+# Learn more about the configuration options: https://reference.trueface.ai/cpp/dev/latest/py/general.html
 options = tfsdk.ConfigurationOptions()
+# The face recognition model to use. Use the most accurate model TFV5.
+options.fr_model = tfsdk.FACIALRECOGNITIONMODEL.TFV5
+# The object detection model to use.
+options.obj_model = tfsdk.OBJECTDETECTIONMODEL.ACCURATE
+# The face detection filter.
+options.fd_filter = tfsdk.FACEDETECTIONFILTER.BALANCED
+# Smallest face height in pixels for the face detector.
+# Can set this to -1 to dynamically change the smallest face height based on the input image size.
+options.smallest_face_height = 40 
+# The path specifying the directory containing the model files which were downloaded.
+options.models_path = os.getenv('MODELS_PATH') or './'
+# Enable vector compression to improve 1 to 1 comparison speed and 1 to N search speed.
+options.fr_vector_compression = False
+# Database management system for the storage of biometric templates for 1 to N identification.
+options.dbms = tfsdk.DATABASEMANAGEMENTSYSTEM.SQLITE
+
+# Encrypt the biometric templates stored in the database
+# If encryption is enabled, must provide an encryption key
+options.encrypt_database.enable_encryption = False
+options.encrypt_database.key = "TODO: Your encryption key here"
+
+# Initialize module in SDK constructor.
+# By default, the SDK uses lazy initialization, meaning modules are only initialized when they are first used (on first inference).
+# This is done so that modules which are not used do not load their models into memory, and hence do not utilize memory.
+# The downside to this is that the first inference will be much slower as the model file is being decrypted and loaded into memory.
+# Therefore, if you know you will use a module, choose to pre-initialize the module, which reads the model file into memory in the SDK constructor.
 options.initialize_module.active_spoof = True
-options.initialize_module.face_recognizer = True
-options.fr_model = tfsdk.FACIALRECOGNITIONMODEL.TFV5 # We will use TFV5 to verify that both images are from the same person
+
+# Options for enabling GPU
+# We will disable GPU inference, but you can easily enable it by modifying the following options
+# Note, you may require a specific GPU enabled token in order to enable GPU inference.
+options.GPU_options = False # TODO: Change this to true to enable GPU
+options.GPU_options.device_index = 0;
+
+gpuModuleOptions = tfsdk.GPUModuleOptions()
+gpuModuleOptions.max_batch_size = 4
+gpuModuleOptions.opt_batch_size = 1
+gpuModuleOptions.max_workspace_size = 2000
+gpuModuleOptions.precision = tfsdk.PRECISION.FP16
+
+# Note, you can set separate GPU options for each GPU supported module
+options.GPU_options.face_detector_GPU_options = gpuModuleOptions
+options.GPU_options.face_recognizer_GPU_options = gpuModuleOptions
+options.GPU_options.mask_detector_GPU_options = gpuModuleOptions
+options.GPU_options.object_detector_GPU_options = gpuModuleOptions
+
+sdk = tfsdk.SDK(options)
+
 
 # options.enable_GPU = True # Use GPU inference to improve speed
 # You will require the GPU SDK and GPU token for this.
