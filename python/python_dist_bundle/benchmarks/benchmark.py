2de2545186f7c32b29300a28f64f71905eac75cf
David Mentis <dmentis@pangiam.com>
2023-10-12 19:54:06 -0400

    SDK-235 benchmark_face_image_orientation_detection
    Benchmark did not previously exist, so added to match c++ version

--- python/python_dist_bundle/benchmarks/benchmark.py
+++ python/python_dist_bundle/benchmarks/benchmark.py
@@ -57,17 +57,15 @@ def benchmark_preprocess_image(license, gpu_options, num_iterations = 200):
                 return
 
     # Time the preprocess_image function
-    # Run our timing code
     stop_watch = Stopwatch()
     for _ in range(num_iterations):
-        sdk.preprocess_image("./headshot.jpg")
+        sdk.preprocess_image(img_path)
     total_time = stop_watch.elapsedTimeMilliSeconds()
     avg_time = total_time / num_iterations
 
     print("Average time preprocess_image JPG image from disk ({}x{}): {} ms | {} iterations".format(
         img.get_width(), img.get_height(), avg_time, num_iterations))
 
-    print('here')
     # Now repeat with encoded image in memory
     size = os.path.getsize(img_path)
     with open(img_path, 'rb') as infile:
@@ -117,6 +115,48 @@ def benchmark_preprocess_image(license, gpu_options, num_iterations = 200):
 
     print("Average time preprocess_image JPG image in memory ({}x{}): {} ms | {} iterations".format(
         img.get_width(), img.get_height(), avg_time, num_iterations))
+
+
+def benchmark_face_image_orientation_detection(license, gpu_options, num_iterations):
+    # Initialize the SDK
+    options = tfsdk.ConfigurationOptions()
+
+    options.models_path = "./"
+    models_path = os.getenv('MODELS_PATH')
+    if models_path:
+        options.models_path = models_path
+
+    options.GPU_options = gpu_options
+
+    sdk = tfsdk.SDK(options)
+
+    is_valid = sdk.set_license(license)
+    if (is_valid is False):
+        print('Error: the provided license is invalid.')
+        exit(1)
+
+    img_path = './headshot.jpg'
+    # Load the image
+    error_code, img = sdk.preprocess_image(img_path)
+    if error_code != tfsdk.ERRORCODE.NO_ERROR:
+        print('Error: could not load the image')
+        return
+
+    if DO_WARMUP:
+        for _ in range(NUM_WARMUP):
+            error_code, flags = sdk.get_face_image_rotation(img)
+            if error_code != tfsdk.ERRORCODE.NO_ERROR:
+                print('Error: Unable to compute face image orientation')
+                return
+
+    stop_watch = Stopwatch()
+    for _ in range(num_iterations):
+        sdk.get_face_image_rotation(img)
+    total_time = stop_watch.elapsedTimeMilliSeconds()
+    avg_time = total_time / num_iterations
+
+    print('Average time face image orientation detection: {} ms  | {} iterations'.format(
+        avg_time, num_iterations))
 
 
 def benchmark_face_landmark_detection(license, gpu_options, num_iterations = 100):
@@ -502,6 +542,7 @@ def main():
         mult_factor = 10
 
     benchmark_preprocess_image(license, gpu_options, 200)
+    benchmark_face_image_orientation_detection(license, gpu_options, 50*mult_factor)
 
 
 if __name__ == '__main__':
