0a939829c3a562ce1f9cdc78280ea11a2b74aa81
David Mentis <dmentis@pangiam.com>
2023-10-13 13:52:50 -0400

    SDK-235 benchmark_mask_detection

--- python/python_dist_bundle/benchmarks/benchmark.py
+++ python/python_dist_bundle/benchmarks/benchmark.py
@@ -358,52 +358,63 @@ def benchmark_spoof_detection(license, gpu_options, num_iterations = 100):
     print("Average time spoof detection: {} ms | {} iterations".format(avg_time, num_iterations))
 
 
-def benchmark_mask_detection(license, gpu_options, batch_size = 1, num_iterations = 100):
+def benchmark_mask_detection(license, gpu_options, batch_size, num_iterations):
+    # Initialize the SDK
     options = tfsdk.ConfigurationOptions()
-    options.models_path = os.getenv('MODELS_PATH') or './'
+
+    options.models_path = "./"
+    models_path = os.getenv('MODELS_PATH')
+    if models_path:
+        options.models_path = models_path
+
     options.GPU_options = gpu_options
 
-    options.smallest_face_height = 40
-    options.initialize_module.face_detector = True
+    initialize_module = tfsdk.InitializeModule()
+    initialize_module.face_detector = True
+    options.initialize_module = initialize_module
 
     sdk = tfsdk.SDK(options)
 
-    is_valid = sdk.set_license(os.environ['TRUEFACE_TOKEN'])
-    if (is_valid == False):
-        print(f"{Fore.RED}Invalid License Provided{Style.RESET_ALL}")
-        print(f"{Fore.RED}Be sure to export your license token as TRUEFACE_TOKEN{Style.RESET_ALL}")
-        quit()
+    is_valid = sdk.set_license(license)
+    if is_valid is False:
+        print('Error: the provided license is invalid.')
+        exit(1)
 
-
+    # Load the image
     ret, img = sdk.preprocess_image("./headshot.jpg")
-    if (ret != tfsdk.ERRORCODE.NO_ERROR):
-        print("There was an error setting the image in the {} method".format(inspect.stack()[0][3]))
-        quit()
-
-    found, fb = sdk.detect_largest_face(img)
-    if found == False:
-        print("Unable to find face in {} method".format(inspect.stack()[0][3]))
-        quit()
-
-    chip = sdk.extract_aligned_face(img, fb)
-    chips = []
-
-    for i in range(batch_size):
-        chips.append(chip)
-
-    # Run our timing code
-    t1 = current_milli_time()
-    for i in range(num_iterations):
-        ret, mask_labels, mask_score = sdk.detect_masks(chips)
-    t2 = current_milli_time()
-
     if ret != tfsdk.ERRORCODE.NO_ERROR:
-        print("Unable to run mask detection in {} method".format(inspect.stack()[0][3]))
+        print('Error: could not load the image')
+        return
 
-    total_time = t2 - t1
+    #
+    # @todo: SDK-235 pybinding does not return an error code, should we add
+    #        to match others in the SDK?
+    #
+    found, face_box_and_landmarks = sdk.detect_largest_face(img)
+    if found is False:
+        print('Unable to detect face in image')
+        return
+
+    chip = sdk.extract_aligned_face(img, face_box_and_landmarks)
+    chips = batch_size*[chip]
+
+    if DO_WARMUP:
+        for _ in range(NUM_WARMUP):
+            error_code, mask_labels, mask_scores = sdk.detect_masks(chips)
+            if error_code != tfsdk.ERRORCODE.NO_ERROR:
+                print('Error: Unable to run mask detection')
+                return
+
+    # Time the mask detector
+    stop_watch = Stopwatch()
+    for _ in range(num_iterations):
+        sdk.detect_masks(chips)
+    total_time = stop_watch.elapsedTimeMilliSeconds()
     avg_time = total_time / num_iterations / batch_size
 
-    print("Average time mask detection: {} ms | batch size = {} | {} iterations".format(avg_time, batch_size, num_iterations))
+    print("Average time mask detection: {} ms | batch size = {} | {} iterations".format(
+        avg_time, batch_size, num_iterations))
+
 
 def benchmark_head_orientation(license, gpu_options, num_iterations = 200):
     # Initialize the SDK
@@ -662,13 +673,13 @@ def main():
     benchmark_head_orientation(license, gpu_options, 500*mult_factor)
     benchmark_face_image_blur_detection(license, gpu_options, 200*mult_factor)
     benchmark_blink_detection(license, gpu_options, 100*mult_factor)
+    benchmark_mask_detection(license, gpu_options, 1, 100*mult_factor)
 
 
 if __name__ == '__main__':
     main()
 
 
-# benchmark_mask_detection(license, gpu_options, 1, 100*mult_factor)
 # benchmark_spoof_detection(license, gpu_options)
 # benchmark_object_detection(license, gpu_options, 100 * mult_factor)
 
